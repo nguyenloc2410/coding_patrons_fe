1a429f1a5d0a8b10d4feea12a7ff8d5304f737ef
nguyenloc <tanlocnguyen2410@gmail.com>
2023-11-07 06:37:03 +0700

    v2

--- src/configs/axios.js
+++ src/configs/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://server-coding.onrender.com/",
   withCredentials: true,
 });
 instance.interceptors.response.use(
